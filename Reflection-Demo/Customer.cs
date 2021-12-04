87cbbe6e4f00512a4b8910943b1dfd41d0f1fa6f
“Arunesh <as8175802986@gmail.com>
2021-12-04 07:37:23 +0530

    [Arunesh]Added: code for Reflection Architechture

---
+++ Reflection-Demo/Customer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection_Demo
+{
+    class Customer
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+        public string ClassName
+        {
+            get;
+            set;
+        }
+        public Customer(int id, string name)
+        {
+            this.Id = id;
+            this.ClassName = name;
+        }
+        public Customer()
+        {
+            this.Id = -1;
+            this.ClassName = string.Empty;
+        }
+        public void PrintId()
+        {
+            Console.WriteLine("Id is:{0}", this.Id);
+        }
+        public void PrintName()
+        {
+            Console.WriteLine("Name is:{0}", this.ClassName);
+        }
+    }
+}
---
+++ Reflection-Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Reflection_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReflectionTest.Test();
+        }
+    }
+}
